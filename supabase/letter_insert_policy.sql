2dff30fbb39bc679f421b03ec4e2a8c9be797817
Sathyasravya Vallabhajyosyula <sathyasravya.v@gmail.com>
2026-03-10 15:17:20 +0100

    chore: remove supabase folder from git tracking while keeping locally

--- supabase/letter_insert_policy.sql
+++
@@ -1,9 +0,0 @@
--- Allow the API generation route to push new letters using the Anon Key
-CREATE POLICY "Allow anon/authenticated insert for letters" 
-ON public.letters FOR INSERT 
-WITH CHECK (true);
-
--- Allow the API generation route to push deliveries using the Anon Key
-CREATE POLICY "Allow anon/authenticated insert for deliveries" 
-ON public.letter_deliveries FOR INSERT 
-WITH CHECK (true);
